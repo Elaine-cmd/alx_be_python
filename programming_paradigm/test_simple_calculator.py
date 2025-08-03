6137c76fa6f31b0ca35c0f9cd9a3ba1be36693d6
Elaine Ngunu <elainewaithere90@gmail.com>
2025-08-03 21:27:03 +0300

    Add thorough unit tests for all SimpleCalculator methods including edge cases

---
+++ programming_paradigm/test_simple_calculator.py
@@ -0,0 +1,40 @@
+import unittest
+from simple_calculator import SimpleCalculator
+
+class TestSimpleCalculator(unittest.TestCase):
+    def setUp(self):
+        """Set up the SimpleCalculator instance before each test."""
+        self.calc = SimpleCalculator()
+
+    def test_add(self):
+        self.assertEqual(self.calc.add(2, 3), 5)
+        self.assertEqual(self.calc.add(-1, 1), 0)
+        self.assertEqual(self.calc.add(-5, -3), -8)
+        self.assertEqual(self.calc.add(0, 0), 0)
+        self.assertEqual(self.calc.add(1.5, 2.5), 4.0)
+
+    def test_subtract(self):
+        self.assertEqual(self.calc.subtract(10, 5), 5)
+        self.assertEqual(self.calc.subtract(0, 0), 0)
+        self.assertEqual(self.calc.subtract(-3, -6), 3)
+        self.assertEqual(self.calc.subtract(5, 10), -5)
+        self.assertEqual(self.calc.subtract(2.5, 1.2), 1.3)
+
+    def test_multiply(self):
+        self.assertEqual(self.calc.multiply(4, 3), 12)
+        self.assertEqual(self.calc.multiply(-2, 3), -6)
+        self.assertEqual(self.calc.multiply(0, 10), 0)
+        self.assertEqual(self.calc.multiply(-4, -5), 20)
+        self.assertEqual(self.calc.multiply(2.5, 4), 10.0)
+
+    def test_divide(self):
+        self.assertEqual(self.calc.divide(10, 2), 5)
+        self.assertEqual(self.calc.divide(9, 3), 3)
+        self.assertEqual(self.calc.divide(-8, 2), -4)
+        self.assertEqual(self.calc.divide(-6, -2), 3)
+        self.assertAlmostEqual(self.calc.divide(7, 2), 3.5)
+        self.assertAlmostEqual(self.calc.divide(1, 3), 1/3)
+
+    def test_divide_by_zero(self):
+        self.assertIsNone(self.calc.divide(5, 0))
+        self.assertIsNone(self.calc.divide(0, 0))
